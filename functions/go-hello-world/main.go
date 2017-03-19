294af095c5d2e97a001895f3541071b04f2f02b5
Scott Ranger <millenniumnight@googlemail.com>
2017-03-19 18:25:15 +0000

    Fix for go hello world due to the nature of the shim

--- functions/go-hello-world/main.go
+++ functions/go-hello-world/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 
 	apex "github.com/apex/go-apex"
 )
 
 func main() {
 	apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
-		fmt.Println("Hello world")
+		fmt.Fprintln(os.Stderr, "Hello world")
 
 		return "Success", nil
 	})
