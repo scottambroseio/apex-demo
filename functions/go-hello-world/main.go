c9ddfe7913bba659cbcf1141c296346a80484c95
Scott Ranger <millenniumnight@googlemail.com>
2017-03-19 19:56:00 +0000

    go function helper method

--- functions/go-hello-world/main.go
+++ functions/go-hello-world/main.go
@@ -8,9 +8,15 @@ import (
 	apex "github.com/apex/go-apex"
 )
 
+// stdout is reserved for the node shim so all messages to the console
+// need to go through stderr
+func printline(message string) {
+	fmt.Fprintln(os.Stderr, message)
+}
+
 func main() {
 	apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
-		fmt.Fprintln(os.Stderr, "Hello world")
+		printline("Hello world")
 
 		return "Success", nil
 	})
